3cf4bd8b80529f5a6792c7c62b7a829cf391bf88
12 <fedor@MacBook-Pro-Fedor.local>
2020-01-27 17:55:25 +0300

    Changed members

--- discord_bot.py
+++ discord_bot.py
@@ -8,12 +8,12 @@ async def on_ready():
     print('Bot is ready.')
 
 @client.event
-async def on_member_join(mebmer):
-    print(f'{mebmer} has joined a server.')
+async def on_member_join(member):
+    print(f'{member} has joined a server.')
 
 @client.event
 async def on_member_remove(member):
-    print(f'{mebmer} has left a server.')
+    print(f'{member} has left a server.')
 
 @client.command()
 async def ping(ctx):
