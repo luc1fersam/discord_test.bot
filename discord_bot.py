c0685bf6a97077f7ed5f2625bfc15d8063963321
xxxcrystalcastles <w3xewellence@yandex.ru>
2020-01-27 23:37:06 +0300

    Added typing, new command avatar

--- discord_bot.py
+++ discord_bot.py
@@ -1,24 +1,39 @@
-import discord
+from typing import Union
+from discord import User, Member, ClientUser
 from discord.ext import commands
+from discord.ext.commands import Context
 
-client = commands.Bot(command_prefix = '.')
+client = commands.Bot(command_prefix='.')
+
 
 @client.event
 async def on_ready():
     print('Bot is ready.')
 
+
 @client.event
 async def on_member_join(member):
     print(f'{member} has joined a server.')
+
 
 @client.event
 async def on_member_remove(member):
     print(f'{member} has left a server.')
 
+
 @client.command()
 async def ping(ctx):
     await ctx.send('Pong!')
 
+
+@client.command(name='avatar')
+async def avatar_command(ctx: Context, user: Union[User, Member, ClientUser]):
+    if not isinstance(user, (User, Member, ClientUser)):
+        return
+
+    avatar_url = str(user.avatar_url_as(format='png'))
+
+    await ctx.send(avatar_url)
 
 
 client.run('NjcwNjgzMDQxNTc1OTkzMzY1.Xix8Zg.vakRr3Uy5SgMDtyFlICqmmi5v24')
